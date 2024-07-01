3a7413bce8af2718faceae88f898a469cfe23e42
AbikGrg <57242140+AbikGrg@users.noreply.github.com>
2024-07-01 00:23:09 -0400

    fixed routing

--- src/components/Dashboard/Sidebar.jsx
+++ src/components/Dashboard/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = ({ sidebarToggle, username }) => {
           </Link>
         </li>
         <li className='mb-2 rounded hover:shadow hover:bg-blue-500 py-2'>
-          <Link to="/weather" className='px-3'>
+          <Link to="/newfeed" className='px-3'>
             <FaNewspaper className='inline-block w-6 h-6 mr-2 -mt-2'></FaNewspaper>
             NewsFeed
           </Link>
